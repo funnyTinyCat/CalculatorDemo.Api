a2487fb197209daff32ed5da6bb323da92d8aacc
Oliver <opaligoric@gmail.com>
2026-05-24 16:42:55 +0200

    First commit

---
+++ CalculatorDemo.Api/Models/Calculator.cs
@@ -0,0 +1,9 @@
+namespace CalculatorDemo.Api.Models;
+
+public class Calculator
+{
+    public int Add(int value1, int value2)
+    {
+        return value1 + value2;
+    }
+}
---
+++ CalculatorDemo.UnitTests/CalculatorFixture.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorDemo.Api.Models;
+
+namespace CalculatorDemo.UnitTests;
+
+public class CalculatorFixture
+{
+    [Fact]
+    public void Add()
+    {
+        // Arrange
+        var value1 = 1;
+        var value2 = 2;
+        var expected = 3;
+
+        var systemUnderTest = new Calculator();
+
+        // Act
+        var actual = systemUnderTest.Add(value1, value2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
