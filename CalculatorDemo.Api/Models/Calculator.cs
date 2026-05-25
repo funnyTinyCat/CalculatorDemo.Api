ffbac46da336d3ffe4dedba40f1499532dfd30ba
Oliver <opaligoric@gmail.com>
2026-05-25 08:05:28 +0200

    Added theory test

--- CalculatorDemo.Api/Models/Calculator.cs
+++ CalculatorDemo.Api/Models/Calculator.cs
@@ -7,6 +7,39 @@ public class Calculator
         return value1 + value2;
     }
 
+    public int Calculate(string operation, int value1, int value2)
+    {
+        if (operation == "Add")
+        {
+            return Add(value1, value2);
+        }
+        else if (operation == "Subtract")
+        {
+            return Subtract(value1, value2);
+        }
+        else if (operation == "Multiply")
+        {
+            return Multiply(value1, value2);
+        }
+        else if (operation == "Divide")
+        {
+            return Divide(value1, value2);
+        } else
+        {
+            throw new InvalidOperationException($"Unsupported operation: {operation}");
+        } 
+    }
+
+    public int Divide(int value1, int value2)
+    {
+        if (value2 == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+
+        return value1 / value2;
+    }
+
     public int Multiply(int value1, int value2)
     {
         return value1 * value2;
--- CalculatorDemo.UnitTests/CalculatorFixture.cs
+++ CalculatorDemo.UnitTests/CalculatorFixture.cs
@@ -56,4 +56,62 @@ public class CalculatorFixture
         // Assert
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void Divide()
+    {
+        // Arrange
+        var value1 = 10;
+        var value2 = 2;
+        var expected = 5;
+
+        var systemUnderTest = new Calculator();
+
+        // Act
+        var actual = systemUnderTest.Divide(value1, value2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsException()
+    {
+        // Arrange
+        var value1 = 10;
+        var value2 = 0;
+
+        var systemUnderTest = new Calculator();
+
+        // Act & Assert
+        Assert.Throws<DivideByZeroException>(() => systemUnderTest.Divide(value1, value2));
+    }
+
+    [Fact]
+    public void Calculate_OnUnknownOperation_ThrowsException()
+    {
+        // Arrange
+        var systemUnderTest = new Calculator();
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => systemUnderTest.Calculate("Unknown", 1, 2));
+    }
+
+
+    [Theory]
+    [InlineData("Add", 1, 2, 3)]
+    [InlineData("Subtract", 5, 3, 2)]
+    [InlineData("Multiply", 5, 3, 15)]
+    [InlineData("Divide", 10, 2, 5)]
+    public void Calculate(string operation, int value1, int value2, int expected)
+    {
+        // Arrange 
+        var systemUnderTest = new Calculator();
+
+        // Act
+        var actual = systemUnderTest.Calculate(operation, value1, value2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 }
